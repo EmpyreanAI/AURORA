d4e65537bed08e3596e486931af0177a8c2d529a
Renato Avellar Nobre <rekanobre@gmail.com>
2020-03-10 22:49:28 -0300

    Adicionado templates de issues

--- sample/insiders/denselstm.py
+++ sample/insiders/denselstm.py
@@ -1,18 +1,17 @@
+from keras.models import load_model
 from .base import BaseInsider, DEFAULT_DIRS
 
-class SesInsider(BaseInsider):
+class DenseLSTMInsider(BaseInsider):
 
     def __init__(self, insider_id, stock, dirs=DEFAULT_DIRS):
-        super(SesInsider, self).__init__(insider_id, stock, dirs)
+        super(DenseLSTMInsider, self).__init__(insider_id, stock, dirs)
         self._log("Initialized")
         self._log("Stocks: {}".format(self.stock_wallet))
+        self.model = load_model('VALE3-model.h5')
 
     def predict_direction(self):
-        dir_amount = len(self._directions)
-        index = random.randrange(dir_amount)
-        directions = self._directions[index]
-        self.log_direction(directions)
-        return directions
+        model = load_model('denselstm.h5')
+        return
 
     def notify(self):
         return self.predict_direction()
@@ -21,4 +20,4 @@ class SesInsider(BaseInsider):
         self._log("({0}): {1}".format(self.stock_name, direction))
 
     def _log(self, msg):
-        print("[SesInsider {}] {}".format(self.insider_id, msg))
+        print("[LSTMInsider {}] {}".format(self.insider_id, msg))
